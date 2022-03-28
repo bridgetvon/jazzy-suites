011c41ec71db43afd5762bd305b8840ca7190322
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2022-03-28 16:25:56 -0600

    refactored navbar with tailwinds, having trouble with bottome border

--- components/Nav.js
+++ components/Nav.js
@@ -1,25 +1,89 @@
 import Link from 'next/link'
 import navStyles from '../styles/Nav.module.css'
-
+import { useState } from 'react'
+import Image from 'next/image'
 
 export default function Nav() {
-  return (
-    <nav className={navStyles.nav}>
-        <ul>
-            <li className={navStyles.icon}>
-               <img src='https://www.nba.com/resources/static/team/v2/jazz/JAZ1718_SeasonTickets_JNoteWhite.png'></img>
-            </li>
-            <li className={navStyles.premiumtitle}>UTAH JAZZ PREMIUM SEATING</li>
-            <li>
-                <a href='#luxury-suites'>Luxury Suites</a>
-            </li>
-            <li>
-               <a href='#exclusive-clubs'>Exclusive Clubs</a>
-            </li>
-            <li>
-               <a href='#contact-us'>Contact Us</a>
-            </li>
-        </ul>
-    </nav>
-  )
+    const [open, setOpen] = useState(false)
+    return (
+        <nav className='flex bg-black items-center flex-wrap inline'>
+            <MobileNav open={open} setOpen={setOpen} />
+            {/* <div className="flex lg:inline-flex lg:w-auto w-full px-3 py-2"> */}
+
+            {/* </div> */}
+            <div className="w-9/12 flex justify-end items-center">
+                <div className="z-50 flex relative w-8 h-8 flex-col justify-between items-center md:hidden" onClick={() => {
+                    setOpen(!open)
+                }}>
+                    {/* hamburger button */}
+                    <span className={`h-1 w-full bg-white rounded-lg transform transition duration-300 ease-in-out ${open ? "rotate-45 translate-y-3.5" : ""}`} />
+                    <span className={`h-1 w-full bg-white rounded-lg transition-all duration-300 ease-in-out ${open ? "w-0" : "w-full"}`} />
+                    <span className={`h-1 w-full bg-white rounded-lg transform transition duration-300 ease-in-out ${open ? "-rotate-45 -translate-y-3.5" : ""}`} />
+                </div>
+                <div className="hidden w-full lg:inline-flex lg:flex-grow lg:w-auto text-yellow ">
+                    <div className='lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto'>
+                        <Image
+                            src='/../public/images/jazzlogo.png'
+                            alt='jazz logo'
+                            width={50}
+                            height={50}
+                        />
+                        <span className='text-white pl-3 tracking-wide flex-start'>UTAH JAZZ PREMIUM SEATING</span>
+                        <Link href='/'>
+                            <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 items-center justify-center hover:bg-green-600 hover:text-white '>
+                                Luxury Suites
+                            </a>
+                        </Link>
+                        <Link href='/'>
+                            <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 items-center justify-center hover:bg-green-600 hover:text-white'>
+                                Exclusive Clubs
+                            </a>
+                        </Link>
+                        <Link href='/'>
+                            <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 items-center justify-center hover:bg-green-600 hover:text-white'>
+                                Contact us
+                            </a>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+    )
 }
+
+function MobileNav({ open, setOpen }) {
+    return (
+        <div className={`absolute top-0 left-0 h-screen w-screen bg-white transform ${open ? "-translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out filter drop-shadow-md `}>
+            <div className="flex items-center justify-center filter drop-shadow-md bg-white h-20"> {/*logo container*/}
+                {/* <a className="text-xl font-semibold" href="/">LOGO</a> */}
+            </div>
+            <div className="flex flex-col ml-4">
+                <a className="text-xl font-medium my-4 text-yellow" href="/about" onClick={() => setTimeout(() => { setOpen(!open) }, 100)}>
+                    About
+                </a>
+                <a className="text-xl font-normal my-4" href="/contact" onClick={() => setTimeout(() => { setOpen(!open) }, 100)}>
+                    Contact
+                </a>
+            </div>
+        </div>
+    )
+}
+
+{/* <nav className='flex p-15 bg-black text-yellow justify-end content-center'>
+<ul>
+    <li className={navStyles.icon}>
+       <img src='https://www.nba.com/resources/static/team/v2/jazz/JAZ1718_SeasonTickets_JNoteWhite.png'></img>
+    </li>
+    <li className='text-white justify-start'>UTAH JAZZ PREMIUM SEATING</li>
+    <li>
+        <a href='#luxury-suites'>Luxury Suites</a>
+    </li>
+    <li>
+       <a href='#exclusive-clubs'>Exclusive Clubs</a>
+    </li>
+    <li>
+       <a href='#contact-us'>Contact Us</a>
+    </li>
+</ul>
+</nav> */}
--- components/Suites.js
+++ components/Suites.js
@@ -3,36 +3,38 @@ import Image from 'next/image'
 
 export default function Suites() {
     return (
-        <div id="mainDiv" className='bg-white'>
-            <div className="grid grid-cols-2 gap-8 text-white bg-black">
-                <div className="max-w-sm rounded overflow-hidden shadow-lg">
-                    <div className="px-6 py-4">
+        <div id="mainDiv" className='bg-white p-15 flex'>
+            <div className="text-white bg-black ml-20 mr-4 my-5">
+                <div className="max-w-sm overflow-hidden shadow-lg">
+                    <div>
                         <Image
                             src='/../public/images/luxury-suites.jpg'
                             alt='Luxury suite picture'
-                            width={450}
+                            width={500}
                             height={220}
                         />
-                        <div className="font-bold text-xl text-yellow mb-2">Luxury Suites</div>
-                        <p className="text-gray-700 text-base">
+                        <div className="font-bold text-xl text-yellow m-2">Luxury Suites</div>
+                        <p className="text-gray-700 text-base p-3">
                             Your suite can enhance your corporate profile and give you an advantage over your competitors. It offers the chance to communicate your message in a luxurious setting while making entertaining as easy as it is elegant. Take advantage of the most unique and exciting way to entertain in Utah by becoming a Vivint Arena luxury suite holder.
                         </p>
-                        <button className='text-yellow'>Learn More</button>
+                        <button className='text-yellow p-5'>Learn More</button>
                     </div>
                 </div>
-                <div className="max-w-sm rounded overflow-hidden shadow-lg">
-                    <div className="px-6 py-4">
+                </div>
+                <div className='text-white bg-black mr-20 my-5'>
+                <div className="max-w-sm overflow-hidden shadow-lg">
+                    <div>
                         <Image
                             src='/../public/images/exclusive-clubs.jpg'
                             alt='Luxury suite picture'
-                            width={450}
+                            width={500}
                             height={220}
                         />
-                        <div className="font-bold text-xl text-yellow mb-2">Exclusive Clubs</div>
-                        <p className="text-gray-700 text-base">
+                        <div className="font-bold text-xl text-yellow m-2">Exclusive Clubs</div>
+                        <p className="text-gray-700 text-base p-3">
                             Your suite can enhance your corporate profile and give you an advantage over your competitors. It offers the chance to communicate your message in a luxurious setting while making entertaining as easy as it is elegant. Take advantage of the most unique and exciting way to entertain in Utah by becoming a Vivint Arena luxury suite holder.
                         </p>
-                        <button className='text-yellow'>Learn More</button>
+                        <button className='text-yellow p-5'>Learn More</button>
                     </div>
                 </div>
             </div>
@@ -40,3 +42,38 @@ export default function Suites() {
 
     )
 }
+
+{/* <div id="mainDiv" className='bg-white p-15'>
+<div className="grid grid-cols-2 gap-1 text-white bg-black m-20">
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <div className="px-6 py-4">
+            <Image
+                src='/../public/images/luxury-suites.jpg'
+                alt='Luxury suite picture'
+                width={450}
+                height={220}
+            />
+            <div className="font-bold text-xl text-yellow mb-2">Luxury Suites</div>
+            <p className="text-gray-700 text-base">
+                Your suite can enhance your corporate profile and give you an advantage over your competitors. It offers the chance to communicate your message in a luxurious setting while making entertaining as easy as it is elegant. Take advantage of the most unique and exciting way to entertain in Utah by becoming a Vivint Arena luxury suite holder.
+            </p>
+            <button className='text-yellow'>Learn More</button>
+        </div>
+    </div>
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <div className="px-6 py-4">
+            <Image
+                src='/../public/images/exclusive-clubs.jpg'
+                alt='Luxury suite picture'
+                width={450}
+                height={220}
+            />
+            <div className="font-bold text-xl text-yellow mb-2">Exclusive Clubs</div>
+            <p className="text-gray-700 text-base">
+                Your suite can enhance your corporate profile and give you an advantage over your competitors. It offers the chance to communicate your message in a luxurious setting while making entertaining as easy as it is elegant. Take advantage of the most unique and exciting way to entertain in Utah by becoming a Vivint Arena luxury suite holder.
+            </p>
+            <button className='text-yellow'>Learn More</button>
+        </div>
+    </div>
+</div>
+</div> */}
