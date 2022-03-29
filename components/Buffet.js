3faea5f494a0ed1aa35145265f32d11a8e3b29fa
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2022-03-29 16:57:55 -0600

    one last photo

--- components/Buffet.js
+++ components/Buffet.js
@@ -5,7 +5,7 @@ export default function Buffet() {
   return (
     <div className='relative'>
     <Image 
-    src='/../public/images/buffet.jpg'
+    src='/buffet.jpg'
     alt='Buffet in the Eide Bailly suite'
     width={950}
     height={450}
